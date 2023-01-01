89ed073a24c16788e58fea3f5acbec416ee53ec0
8bd8 <glibc@foxmail.com>
2023-01-01 18:01:13 +0800

    Update qq.py

--- qq.py
+++ qq.py
@@ -1,9 +1,8 @@
 import requests,os,time,html2text
 from bs4 import BeautifulSoup as bs
 starttime=time.time()
-def timex():return time.strftime('%Y-%m-%d %H:%M:%S:{}'.format(int(time.time()*1000)%1000))
-url1='https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list?sub_srv_id=24hours&srv_id=pc&offset=0&limit=160&strategy=1&ext={"pool":["top","hot"],"is_filter":7,"check_type":true}'
-headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"}
+url1='https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list?sub_srv_id=24hours&srv_id=pc&offset=0&limit=190&strategy=1&ext={"pool":["top","hot"],"is_filter":7,"check_type":true}'
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
 qq1=requests.get(headers=headers,url=url1).json()
 datalist=[]
 for i in qq1["data"]["list"]:
@@ -21,8 +20,13 @@ for i in datalist:
         s=f"<h1>{i[0]}</h1>"+ss
         s=s.replace("//inews.gtimg.com","https://inews.gtimg.com").replace("</img>","</img><br/>")
         s=html2text.html2text(s)
-        if len(s.split())<=4:continue
-        if os.path.exists(f"{time.strftime('%Y-%m-%d')}/{i[0]}.md"):continue
+        if len(s.split())<=3:
+            print(f"INVALID\n{i[0]}\n{i[1]}\n\n")
+            continue
+        if os.path.exists(f"{time.strftime('%Y-%m-%d')}/{i[0]}.md"):
+            print(f"EXIST\n{i[0]}\n{i[1]}\n\n")
+            continue
         with open(f"{time.strftime('%Y-%m-%d')}/{i[0]}.md","w",encoding="utf-8") as x:x.write(s)
+        print(f"SUCCESS\n{i[0]}\n{i[1]}\n\n")
 
-    except:pass
+    except:print(f"ERROR\n{i[0]}\n{i[1]}\n\n")
